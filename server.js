beded5f5bcd3f756d3789cb32359a34da1070427
jose.danilo.m.neto <jose.danilo.m.neto@avanade.com>
2024-09-05 08:32:20 -0300

    changes

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const { connectToDatabase, client } = require('./db');
 const app = express();
 const port = process.env.PORT;
 const secretKey = process.env.JWT_SECRET_KEY;
+const hmacKey = process.env.HMAC_SECRET_KEY;
+const apiKey = process.env.API_KEY;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -36,20 +38,46 @@ app.post('/api/v1/register', async (req, res) => {
     res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
-app.post('/api/v1/login', express.urlencoded({ extended: true }), async (req, res) => {
-    console.log('User-Agent:', req.headers['user-agent']);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function verifySignature(req, res, next) {
+    const signature = req.headers['x-signature'];
+    const apiKeyHeader = req.headers['x-api-key'];
+    const userAgent = req.headers['user-agent'];
+
+    if (apiKeyHeader !== apiKey || userAgent !== 'CertQuiz') {
+        return res.status(403).json({ message: 'Acesso negado' });
+    }
+
+    const payload = new URLSearchParams(req.body).toString();
+    const hmac = crypto.createHmac('sha256', hmacKey);
+    const digest = hmac.update(payload).digest('hex');
+
+    if (signature !== digest) {
+        return res.status(403).json({ message: 'Assinatura inválida' });
+    }
+
+    next();
+}
+
+app.post('/api/v1/login', verifySignature, async (req, res) => {
     const { username, password } = req.body;
     const user = await app.locals.database.collection('users').findOne({ username });
     if (!user) {
-        return res.status(401).json({ message: 'Senha incorreta.' });
+        return res.status(401).json({ message: 'Usuário e/ou senha incorreta.' });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-        return res.status(401).json({ message: 'Senha incorreta' });
+        return res.status(401).json({ message: 'Usuário e/ou senha incorreta' });
     }
     const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
     const expirationDate = new Date(Date.now() + 3600000); // 1 hora a partir de agora
     res.status(201).json({ token, expiresAt: expirationDate });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
 
 // Rota protegida
